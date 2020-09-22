b070974f24453dddbb78704a95ed42b0688dba56
woodfordtim <woodfordtim@gmail.com>
2020-09-22 23:17:19 +0000

    make correction to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -90,7 +90,7 @@ if development:
 
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATEBASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 
